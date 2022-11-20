dccc93b543b4ed51acc424898a61f679fcbab429
Palladd <90500520+Palladd@users.noreply.github.com>
2022-11-20 22:59:57 +0100

    Add files via upload

---
+++ forEach.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ogorki[] = {13, 123,21, 5 ,1245, 242,1, 412};
+
+    for (int iterator : ogorki) {
+        cout << iterator << endl;
+    }
+return 0;
+}
